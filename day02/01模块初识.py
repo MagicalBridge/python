9c8fd27ac1df367dce3ce27a8ddb1a9632be2d60
MagicalBridge <450793585@qq.com>
2018-03-02 23:50:31 +0800

    模块的修改

--- day02/01模块初识.py
+++ day02/01模块初识.py
@@ -1,3 +1,20 @@
 # Author:Louis Chu
+
+# Python的强大之处在于他有非常丰富和强大的标准库和第三方库，
+# 几乎你想实现的任何功能都有相应的Python库支持，
+# 现在，我们先来象征性的学2个简单的。
+
 # 模块是一个功能的集合
 # 导入模块的时候他会
+
+import sys
+
+print(sys.argv)
+
+
+# 输出
+# $ python
+# test.py
+# helo
+# world
+# ['test.py', 'helo', 'world']  # 把执行脚本时传递的参数获取到了
