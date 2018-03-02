d82e065e6d5640aa92850606fc066257ebac9aeb
Louis <450793585@qq.com>
2018-03-02 22:35:37 +0800

    提交代码  优化学习目录

--- day01/12for循环.py
+++ day01/12for循环.py
@@ -1,7 +1,30 @@
 # Author:Louis Chu
 # 这是一个简单的for循环 在这个循环中 i 是一个临时的变量
-for i in range(10):
-    print("loop", i);
-# 这个for 循环增加了步长的概念，也就是隔几个打印一下
-for i in range(0, 10, 2):
-    print("for2", i)
+# 这里面的 range 我们可以理解为循环10次。
+# 打印的结果是 0-9
+# for i in range(10):
+#     print("loop", i)
+
+# 打印结果：
+# loop 0
+# loop 1
+# loop 2
+# loop 3
+# loop 4
+# loop 5
+# loop 6
+# loop 7
+# loop 8
+# loop 9
+
+
+# 下面这个for循环增加了 步长 的概念，也就是隔几个打印一下
+# 默认的步长是1,可以不写
+for j in range(0, 10, 2):
+    print("for循环升级版本", j)
+
+# for循环升级版本 0
+# for循环升级版本 2
+# for循环升级版本 4
+# for循环升级版本 6
+# for循环升级版本 8
--- day01/13使用for循环猜名字.py
+++ day01/13使用for循环猜名字.py
@@ -1,16 +1,17 @@
 # Author:Louis Chu
 
-age_of_oldboy = 56  # 定义的比较的变量
+# 我们可以使用for循环 while循环的猜名字游戏做一个改版
+age_of_louis = 23  # 定义的比较的变量
 for i in range(3):  # 使用for 循环的时候不需要在外面定义相应的变量
     guess_age = int(input("guess age:"))  # 验证输入额内容效果;
-    if guess_age == age_of_oldboy:
-        print("yes,you got it");
-        break;  # 如果猜对了同样的跳出循环
-    elif guess_age > age_of_oldboy:
+    if guess_age == age_of_louis:
+        print("yes,you got it")
+        break  # 如果猜对了同样的跳出循环
+    elif guess_age > age_of_louis:
         print("think smaller")
     else:
         print("think bigger")
         # 当然我们可以对代码进行优化一下详见下一个文件
         # Author:Louis Chu
-else:  # 这个只有在for 正常的执行的时候才会正常执行 跳出的时候不会执行这句话
+else:  # 这个只有在for 正常的执行的时候才会正常执行 break跳出的时候不会执行这句话
     print("you have try many times funk off")
