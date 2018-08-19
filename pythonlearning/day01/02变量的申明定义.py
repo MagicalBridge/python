5b5921a88605bf351909878842c55683a6abd8e3
褚鹏飞 <540788769@qq.com>
2018-08-19 16:03:01 +0800

    去掉分号

--- pythonlearning/day01/02变量的申明定义.py
+++ pythonlearning/day01/02变量的申明定义.py
@@ -4,13 +4,13 @@
 #1.什么是变量
 #2.变量的命名规则
 #3.常量的命名规则
-name = "louis"; #py中声明变量不需要关键字声明
-print("我的名字是",name); #我的名字是 louis
+name = "louis" #py中声明变量不需要关键字声明
+print("我的名字是",name) #我的名字是 louis
 
 name2 = name; #将louis 这个变量赋值给name2
 name = "paoche ge"; #重新赋值name ="paoche ge"
 
-print(name,name2); #paoche ge louis
+print(name,name2) #paoche ge louis
 
 # 上述程序为什么会打印这样的结果
 # name被赋值之后 将name的值赋值给了name2
--- pythonlearning/day01/03交互程序.py
+++ pythonlearning/day01/03交互程序.py
@@ -2,8 +2,8 @@
 # 下面是一个交互程序,程序运行的时候需要输入用户名和密码
 # 打印出用户名和密码；
 # 使用input的关键字启动交互的程序
-username = input("username:");
-password = input("password:");
+username = input("username:")
+password = input("password:")
 print(username, password);
 
 # 上面的代码的运行的逻辑是这样的python 中读到 input 这个函数（暂时认为是内置的函数）
